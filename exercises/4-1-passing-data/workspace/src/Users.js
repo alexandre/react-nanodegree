3c7a545287c10a6f5eec4089dd65bf1355138625
Alexandre <0x41e@protonmail.ch>
2018-09-21 09:31:17 -0300

    Add exercise 4.1

---
+++ exercises/4-1-passing-data/workspace/src/Users.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class User extends Component {
+    render() {
+        return (
+            <li key={this.props.user.id}>
+                {`${this.props.user.name}'s favorite movie is ${this.props.movie.name}`}
+            </li>
+        )
+    }
+}
+
+
+class UserList extends Component {
+    render() {
+        const { profiles, users, movies } = this.props;
+        return (
+            <ol>
+                {profiles.map(
+                    (profile) => (
+                        <User key={profile.id}
+                            user={users[Number(profile.userID)]}
+                            movie={movies[Number(profile.favoriteMovieID)]} />
+                    )
+                )}
+            </ol>
+        )
+    }
+}
+
+export default UserList;
